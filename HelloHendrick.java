5e5e8ba8572bf150c44bbfcea6078b6eb0d85ab0
nigelhaim <84355145+nigelhaim@users.noreply.github.com>
2022-01-30 20:43:35 +0800

    Update HelloHendrick.java

--- HelloHendrick.java
+++ HelloHendrick.java
@@ -1,6 +1,8 @@
 public class HelloHendrick {
     public static void main(String[] args) {
         System.out.print("Hello Hendrick this is the first test");
+        System.out.print("This is for the test of the merge commit");
+        System.out.print("Conflicting merge");
     }   
     
 }
