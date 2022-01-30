c5e7b4cff496ddd141b8d9f8b8f1e31fefc919ce
nigelhaim <nigelsebastian21@gmail.com>
2022-01-30 20:46:39 +0800

    conflicting

--- HelloHendrick.java
+++ HelloHendrick.java
@@ -1,6 +1,8 @@
 public class HelloHendrick {
     public static void main(String[] args) {
         System.out.print("Hello Hendrick this is the first test");
+
+        System.out.print("This will conflict merge");
     }   
     
 }
