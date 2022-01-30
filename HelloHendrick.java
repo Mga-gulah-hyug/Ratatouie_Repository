952fedf3c47568099d374c31eabbf95904c5e367
nigelhaim <nigelsebastian21@gmail.com>
2022-01-30 20:48:02 +0800

    git push fixed

--- HelloHendrick.java
+++ HelloHendrick.java
@@ -1,13 +1,12 @@
 public class HelloHendrick {
     public static void main(String[] args) {
         System.out.print("Hello Hendrick this is the first test");
-<<<<<<< HEAD
 
         System.out.print("This will conflict merge");
-=======
+
         System.out.print("This is for the test of the merge commit");
         System.out.print("Conflicting merge");
->>>>>>> 5e5e8ba8572bf150c44bbfcea6078b6eb0d85ab0
+
     }   
     
 }
